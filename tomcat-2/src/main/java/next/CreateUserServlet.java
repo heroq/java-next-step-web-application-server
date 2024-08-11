24771c100df8230f24e19805287e7b197013f90c
heroq <96118954+heroq@users.noreply.github.com>
2024-08-11 17:01:55 +0900

    :sparkles: Tomcat 초반 서블릿 추가

---
+++ tomcat-2/src/main/java/next/CreateUserServlet.java
@@ -0,0 +1,26 @@
+package next;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import db.DataBase;
+import model.User;
+
+@WebServlet("/user/create")
+public class CreateUserServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = new User(
+			request.getParameter("userId"),
+			request.getParameter("password"),
+			request.getParameter("name"),
+			request.getParameter("email"));
+		DataBase.addUser(user);
+		response.sendRedirect("/user/list");
+	}
+}
---
+++ tomcat-2/src/main/java/next/ListUserServlet.java
@@ -0,0 +1,22 @@
+package next;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import db.DataBase;
+
+@WebServlet("/user/list")
+public class ListUserServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("users", DataBase.findAll());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/user/list.jsp");
+		dispatcher.forward(request, response);
+	}
+}
